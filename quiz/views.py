c50bde0274d4f14a38f39fd9e5b185e56f4feb58
Abir Sarkar <webdev.abir@gmail.com>
2025-09-08 19:01:27 +0530

    added quiz/models

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, get_object_or_4_4, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.db.models import Sum
